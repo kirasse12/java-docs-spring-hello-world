ce786a3f9dbebb188ced9cea4bb513e9f8a0f1b1
Azdine_kirasse <92144081+kirasse12@users.noreply.github.com>
2023-01-27 14:27:48 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String sayHello() {
-		return "Hello  nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn okWorld!";
+		return "booooooooooooonnnjour les amis!";
 	}
 }
